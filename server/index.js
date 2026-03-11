821687f06c075d86be710bcedf3dac7f8fc61912
adyy_reaa <156661529+AdysonReales@users.noreply.github.com>
2026-03-12 07:37:26 +0800

    Added 0.0.0.0 in index.js

--- server/index.js
+++ server/index.js
@@ -40,4 +40,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 // --- START SERVER ---
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
